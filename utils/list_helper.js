b4732dc9e821bddb618d87d2fd5e877e6a037816
amirthapamagar2053 <amirthapa2019@gmail.com>
2022-09-04 13:22:09 +0545

    feat:will finish 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,20 @@ const dummy = (blogs) => {
     return 1
   }
   
-  module.exports = {
-    dummy
+  
+const totalLikes = (blogs) =>{
+  if(blogs.length === 1){
+    return blogs[0].likes
   }
+  else{
+    let totallike=0;
+    totallike=blogs.reduce((sum,blogs) => sum+blogs.likes,0)
+    return totallike
+  }
+}
+
+
+
+module.exports = {
+  dummy,totalLikes
+}
